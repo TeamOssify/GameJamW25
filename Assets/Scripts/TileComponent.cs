095d4c943fa578be06114df34e3acb4e6ef9ef8f
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2025-02-24 20:10:02 -0500

    Working on tile selection

--- Assets/Scripts/TileComponent.cs
+++ Assets/Scripts/TileComponent.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
-public class TileComponent : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class TileComponent : MonoBehaviour {
+    private Camera _mainCamera;
+    private Tilemap _tileMap;
+
+    internal void Start() {
+        _mainCamera = Camera.main;
+        _tileMap = gameObject.GetComponent<Tilemap>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    internal void OnMouseUpAsButton() {
+        var mousePos = Mouse.current.position;
+        var mouseVec = new Vector3(mousePos.x.value, mousePos.y.value);
+        var worldPos = _mainCamera.ScreenToWorldPoint(mouseVec);
+
+        var cellPos = _tileMap.WorldToCell(worldPos);
+        var hasTile = _tileMap.HasTile(cellPos);
+        Debug.Log($"{worldPos} - {cellPos} {hasTile}");
     }
 }
