b26422ce048ac6178d8839a3ab3f2e03fc35e8a0
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2025-02-25 19:17:04 -0500

    Enemy movement maybe?

--- Assets/Scripts/Enemy/EnemyHandlerComponent.cs
+++ Assets/Scripts/Enemy/EnemyHandlerComponent.cs
@@ -1,7 +1,41 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHandlerComponent : MonoBehaviour {
-    void Start() { }
+    [SerializeField]
+    private TileComponent tileComponent;
 
-    void Update() { }
+    private Vector3Int[] _pointsOfInterest;
+
+    private Dictionary<Vector3Int, EnemyComponent> _enemyGridPositions = new();
+    private Dictionary<Vector3Int, EnemyComponent> _futureEnemyGridPositions = new();
+
+    public void UpdatePointsOfInterest() {
+
+    }
+
+    public void ComputeEnemyMoves() {
+        var takenPoints = new HashSet<Vector3Int>();
+        _futureEnemyGridPositions.Clear();
+
+        foreach (var enemy in _enemyGridPositions.Values) {
+            var movePoint = enemy.ComputeNextMove(tileComponent, _pointsOfInterest, Array.Empty<Vector3Int>(), takenPoints);
+            takenPoints.Add(movePoint);
+            _futureEnemyGridPositions.Add(movePoint, enemy);
+        }
+    }
+
+    public void MoveEnemies() {
+        foreach (var (gridPos, enemy) in _futureEnemyGridPositions) {
+            if (!tileComponent.TryGetWorldPositionForTileCenter(gridPos, out var worldPos)) {
+                Debug.LogError($"Failed to get new world pos for enemy at {gridPos}!");
+                continue;
+            }
+
+            enemy.Move(worldPos, gridPos);
+        }
+
+        (_enemyGridPositions, _futureEnemyGridPositions) = (_futureEnemyGridPositions, _enemyGridPositions);
+    }
 }
