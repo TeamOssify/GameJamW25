dd1d84c8e227f85fff80519d63ed71558a162087
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2025-02-27 23:09:36 -0500

    Cleanup subwave

--- Assets/Scripts/GameLogic/Models/SubWave.cs
+++ Assets/Scripts/GameLogic/Models/SubWave.cs
@@ -3,12 +3,6 @@ using UnityEngine;
 
 [Serializable]
 public class SubWave : MonoBehaviour {
-    public uint SpawnIndex => spawnIndex;
-
-    [SerializeField]
-    [Tooltip("Used to control when a subwave spawn occurs. Ex: 2 will not spawn until 1 is clear.")]
-    private uint spawnIndex;
-
     public EnemyComponent EnemyToSpawn => enemyToSpawn;
 
     [SerializeField]
@@ -18,10 +12,4 @@ public class SubWave : MonoBehaviour {
 
     [SerializeField]
     private uint enemyCount;
-
-    [Tooltip("Forces all enemies to be spawned at the same portal instead of randomly.")]
-    public bool ForceSpawnTogether => forceSpawnTogether;
-
-    [SerializeField]
-    private bool forceSpawnTogether;
 }
